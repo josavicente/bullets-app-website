0c9ea28e1b147010df60488850d61836cc82b020
Josa Vicente <jvicenpe@gmail.com>
2023-06-04 20:24:04 +0200

    Copys de las características

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        primary: colors.indigo,
+        secondary: colors.teal,
+      },
       fontFamily: {
         sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
       },
